71a2ac73f7d9cbf53b6d1af786edef8789d6b4ce
Luciana Muniz <lumunizf@gmail.com>
2018-11-16 11:00:26 -0200

    Update files

--- capybara/tests/features/support/env.rb
+++ capybara/tests/features/support/env.rb
@@ -1,5 +1,10 @@
+require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
+
+Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
 
 Capybara.configure do |config|
     # selenium      selenium_chrome     selenium_chrome_healess
