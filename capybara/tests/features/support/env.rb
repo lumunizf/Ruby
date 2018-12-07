81392e44cfff687eb264774bdc58226289d876aa
Luciana Muniz <lumunizf@gmail.com>
2018-12-07 19:26:16 -0200

    Update files

--- capybara/tests/features/support/env.rb
+++ capybara/tests/features/support/env.rb
@@ -1,5 +1,6 @@
 require 'capybara/cucumber'
 require 'selenium-webdriver'
+#require 'chromedriver/helper'
 
 Capybara.configure do |config|
     # selenium      selenium_chrome     selenium_chrome_healess
@@ -10,3 +11,9 @@ Capybara.configure do |config|
     # Configuração do tempo padrão para aguardar o carregamento dos elementos na tela (contagem por segundos)
     config.default_max_wait_time = 5
 end
+
+Capybara.register_driver :chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+
+Capybara.javascript_driver = :chrome
